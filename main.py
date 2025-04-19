77d676cbf937160ab57399389b466264f1939fad
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-19 14:46:39 +0100

    page title configurations

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import json
+import os
+
+#page config and page titles
+st.set_page_config(page_title = "Simple Finance App", page_icon = "💰",layout = "wide")
