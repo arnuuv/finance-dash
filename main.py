a74968e709a32fb80ad66dbc675359552e8e6175
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-19 16:28:02 +0100

    add keyword to category function

--- main.py
+++ main.py
@@ -25,10 +25,15 @@ def save_categories():
     
 def categorize_transactions(df):
   df["Category"] = "Uncategorized"
-  for category,keyword in st.session_state.categories.items():
+  for category,keywords in st.session_state.categories.items():
     if category == "Uncategorized" or not keywords:
       continue
-    
+    lowered_keywords = [keyword.lower().strip() for keyword in keywords]
+    for idx, row in df.iterrows():
+      details = row["Details"].lower().strip()
+      if details in lowered_keywords:
+        df.at[idx, "Category"] = category
+  return df 
     
     
 def load_transactions(file):
@@ -38,10 +43,17 @@ def load_transactions(file):
     df["Amount"] = df["Amount"].str.replace(",","").astype(float)
     df["Date"] = pd.to_datetime(df["Date"], format = "%d %b %Y" )
     st.write(df)
-    return df
+    return categorize_transactions(df)
   except Exception as e:
     st.error(f"Error processing file: {str(e)}")
     return None
+def add_keyword_to_category(category,keyword):
+  keyword = keyword.strip()
+  if keyword and keyword not in st.session_state.categories[category]:
+    st.session_state.categories[category].append(keyword)
+    save_categories()
+    return True
+  return False
 
 def main():
   st.title("Simple Finance Dashboard")
